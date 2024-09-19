e0e0b532861ac5ae896c24685d110106e75d5a85
cumtbhl <2213538105@qq.com>
2024-09-19 10:59:12 +0800

    9.19-对Lab1的修改（增加了备注）

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -8,12 +8,16 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   Writer& bytes_writer = output_.writer();
   //unacceptable_index:Reassembler能够接受的索引上限
   //第一种情况：first_index > unacceptable_index
+  //*****如果first_index + data.size() <= expecting_index_的情况呢？？？
   if ( const uint64_t unacceptable_index = expecting_index_ + bytes_writer.available_capacity();
        bytes_writer.is_closed() || bytes_writer.available_capacity() == 0 || first_index >= unacceptable_index )
     return; 
 
   //第二种情况：first_index < unacceptable_index
   //           同时 first_index + data.size() > unacceptable_index，那么保留[first_index,unacceptable_index)
+  //******这里如果first_index + data.size() = unacceptable_index
+  //那么data.resize( unacceptable_index - first_index )这一步不会改变data的长度，
+  //但把is_last_substring置为false了？？？？？
   else if ( first_index + data.size() >= unacceptable_index ) {
     is_last_substring = false;                       
     data.resize( unacceptable_index - first_index ); 
@@ -23,6 +27,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     cache_bytes( first_index, move( data ), is_last_substring );
   else
     //这里是不是还要有去重操作？
+    //***** 这里好像默认了first_index + data.size() > expecting_index_
     push_bytes( first_index, move( data ), is_last_substring );
   flush_buffer();
 }
@@ -49,40 +54,39 @@ void Reassembler::push_bytes( uint64_t first_index, string data, bool is_last_su
 void Reassembler::cache_bytes( uint64_t first_index, string data, bool is_last_substring )
 {
   auto end = unordered_bytes_.end();
-  //lower_bound ： 在指定范围内找到第一个不满足指定比较条件的位置
-  //left ： left的 get<0>(e) + get<1>(e).size() >= first_index
+  //left 是第一个结束位置大于或等于 first_index 的缓存片段。
   auto left = lower_bound( unordered_bytes_.begin(), end, first_index, []( auto&& e, uint64_t idx ) -> bool {
     return idx > ( get<0>( e ) + get<1>( e ).size() );
   } );
 
-  //std::upper_bound ：用于在指定范围内找到第一个不满足指定比较条件的位置。
-  //right ： right的 get<0>(e) <= first_index
+  //返回第一个起始位置大于 first_index + data.size() 的缓存片段的迭代器
   auto right = upper_bound( left, end, first_index + data.size(), []( uint64_t nxt_idx, auto&& e ) -> bool {
     return nxt_idx < get<0>( e );
-  } ); // 注意一下：right 指向的是待合并区间右端点的下一个元素
+  } ); 
 
+  //处理与 left 片段的关系：检查新片段是否与 left 片段重叠或相邻，并据此调整新片段或跳过插入
   if ( const uint64_t next_index = first_index + data.size(); left != end ) {
-    //将 left 指向的 tuple 解构为 l_point（起始索引）、dat（数据内容）和第三个元素 _（此处不关心）。
     auto& [l_point, dat, _] = *left;
-    //目标数据段 完全包含在数据段 [l_point, r_point)中，说明数据已经存在于已有段中，直接 return
     if ( const uint64_t r_point = l_point + dat.size(); first_index >= l_point && next_index <= r_point )
       return; 
-    //
     else if ( next_index < l_point ) {
-      right = left;                                                      // data 和 dat 没有重叠部分
-    } else if ( !( first_index <= l_point && r_point <= next_index ) ) { // 重叠了
-      if ( first_index >= l_point ) {                                    // 并且 dat 没有被完全覆盖
+      right = left;                                                      
+    } 
+    else if ( !( first_index <= l_point && r_point <= next_index ) ) { 
+      if ( first_index >= l_point ) {                                    
         data.insert( 0, string_view( dat.c_str(), dat.size() - ( r_point - first_index ) ) );
-      } else {
+      } 
+      else {
         data.resize( data.size() - ( next_index - l_point ) );
-        data.append( dat ); // data 在前
+        data.append( dat ); 
       }
       first_index = min( first_index, l_point );
     }
   }
 
+  //处理与 prev(right) 片段的关系：如果 right 不是 left（即存在至少一个片段在 left 和 right 之间），
+  //则检查新片段是否与 prev(right) 片段相邻并可能重叠，并据此合并片段。
   if ( const uint64_t next_index = first_index + data.size(); right != left && !unordered_bytes_.empty() ) {
-    // 如果 right 指向 left，表示两种可能：没有重叠区间、或者只需要合并 left 这个元素
     auto& [l_point, dat, _] = *prev( right );
     if ( const uint64_t r_point = l_point + dat.size(); r_point > next_index ) {
       data.resize( data.size() - ( next_index - l_point ) );
@@ -90,10 +94,13 @@ void Reassembler::cache_bytes( uint64_t first_index, string data, bool is_last_s
     }
   }
 
+  //清理旧片段：删除 left 到 right（不包括 right）之间的所有旧片段，并更新相关计数器和标志。
   for ( ; left != right; left = unordered_bytes_.erase( left ) ) {
     num_bytes_pending_ -= get<1>( *left ).size();
     is_last_substring |= get<2>( *left );
   }
+  
+  //在清理后的位置插入新片段，包括其起始索引、数据和 is_last_substring 标志。
   num_bytes_pending_ += data.size();
   unordered_bytes_.insert( left, { first_index, move( data ), is_last_substring } );
 }
