4a3089dc8aa18f3adecb3ae0b3333819fcb1c555
cumtbhl <2213538105@qq.com>
2024-09-20 17:46:27 +0800

    Implement Lab2 assignment

--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -3,6 +3,7 @@
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include <optional>
 
 class TCPReceiver
 {
@@ -10,16 +11,14 @@ public:
   // Construct with given Reassembler
   explicit TCPReceiver( Reassembler&& reassembler ) : reassembler_( std::move( reassembler ) ) {}
 
-  /*
-   * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
-   * at the correct stream index.
-   */
+  //该方法接收 TCPSenderMessage 类型的消息，
+  //并将其有效负载插入到 Reassembler 中，确保数据按正确的流索引进行重组。
   void receive( TCPSenderMessage message );
 
-  // The TCPReceiver sends TCPReceiverMessages to the peer's TCPSender.
+  //该方法生成并返回一个 TCPReceiverMessage，用于发送给对端的 TCP 发送者
   TCPReceiverMessage send() const;
 
-  // Access the output (only Reader is accessible non-const)
+  //这些方法提供了对重组器状态的访问，以便进行读取和写入操作
   const Reassembler& reassembler() const { return reassembler_; }
   Reader& reader() { return reassembler_.reader(); }
   const Reader& reader() const { return reassembler_.reader(); }
@@ -27,4 +26,6 @@ public:
 
 private:
   Reassembler reassembler_;
+  //表示接收方是否已经接收到有效的初始序列号。
+  std::optional<Wrap32> ISN_ {};
 };
