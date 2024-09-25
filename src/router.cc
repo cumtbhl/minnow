79189ffee6052eaa2b862c1924a1320c1aa504b2
cumtbhl <2213538105@qq.com>
2024-09-25 21:52:22 +0800

    24/9/25 21:52 完成Lab6

--- src/router.cc
+++ src/router.cc
@@ -2,15 +2,10 @@
 
 #include <iostream>
 #include <limits>
+#include <ranges>
 
 using namespace std;
 
-// route_prefix: The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
-// prefix_length: For this route to be applicable, how many high-order (most-significant) bits of
-//    the route_prefix will need to match the corresponding bits of the datagram's destination address?
-// next_hop: The IP address of the next hop. Will be empty if the network is directly attached to the router (in
-//    which case, the next hop address should be the datagram's final destination).
-// interface_num: The index of the interface to send the datagram out on.
 void Router::add_route( const uint32_t route_prefix,
                         const uint8_t prefix_length,
                         const optional<Address> next_hop,
@@ -20,11 +15,54 @@ void Router::add_route( const uint32_t route_prefix,
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
 
-  // Your code here.
+  router_table_.emplace( prefix_info( prefix_length, route_prefix ), make_pair( interface_num, next_hop ) );
 }
 
-// Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
+
 void Router::route()
 {
-  // Your code here.
+  //  ranges::for_each : 遍历 _interfaces 中的每个网络接口
+  //  intfc : 当前遍历到的网络接口的引用
+  ranges::for_each( _interfaces, [this]( auto& intfc ) {
+
+    //  获取当前接口接收到的数据报队列的引用。
+    auto& incoming_dgrams = intfc->datagrams_received();
+
+    //  遍历所有网络接口，检查是否有新数据报，并处理新的数据报
+    while ( !incoming_dgrams.empty() ) { 
+      //  获取队列中的第一个数据报
+      auto& dgram = incoming_dgrams.front();
+      //  查找当前数据报对应的路由项
+      const auto table_iter = find_export( dgram.header.dst );
+      //  如果没找到路由项，或者该数据包的TTL <= 1，则应该丢弃该数据报
+      if ( table_iter == router_table_.cend() || dgram.header.ttl <= 1 ) {
+        incoming_dgrams.pop(); // TTL == 0 || (TTL - 1) == 0
+        continue;              // 无法路由或 ttl 为 0 的数据报直接抛弃
+      }
+      --dgram.header.ttl;
+      //  由于ttl修改了，需要调用 compute_checksum() 方法以重新计算数据报的校验和
+      dgram.header.compute_checksum();
+      //  解构 table_iter 指向的路由条目
+      //  获取该数据报应该发送到的网络接口编号interface_num、下一跳地址network_addr
+      const auto& [interface_num, network_addr] = table_iter->second;
+
+      interface( interface_num )
+        ->send_datagram(
+          dgram,
+          network_addr.has_value()
+            ? *network_addr
+            : Address::from_ipv4_numeric( dgram.header.dst ) ); // 没有下一跳时，表示位于同一个网络中，直接交付
+      incoming_dgrams.pop();                                    // 已转发数据报，从缓冲队列中弹出
+    }
+  } );
+}
+
+Router::routerT::const_iterator Router::find_export( const uint32_t target_dst ) const
+{
+  //  遍历 router_table_ 中的所有条目，对每个条目进行 target_dst & mask_ == netID_？
+  //  如果当前item 使得 lambda 表达式 为 true，则返回指向该item的迭代器
+  //  如果没找到匹配的，则返回router_table_.end()
+  return ranges::find_if( router_table_, [&target_dst]( const auto& item ) -> bool {
+    return ( target_dst & item.first.mask_ ) == item.first.netID_;
+  } );
 }
--- src/router.hh
+++ src/router.hh
@@ -1,38 +1,76 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <optional>
+#include <compare>
 
 #include "exception.hh"
 #include "network_interface.hh"
 
-// \brief A router that has multiple network interfaces and
-// performs longest-prefix-match routing between them.
 class Router
 {
 public:
-  // Add an interface to the router
-  // \param[in] interface an already-constructed network interface
-  // \returns The index of the interface after it has been added to the router
+
+  //  添加interface到_interfaces中，返回该interface在_interfaces中的索引
   size_t add_interface( std::shared_ptr<NetworkInterface> interface )
   {
     _interfaces.push_back( notnull( "add_interface", std::move( interface ) ) );
     return _interfaces.size() - 1;
   }
 
-  // Access an interface by index
+  //  获取 _interfaces中索引为 N 的网络接口，返回指向它的shared_ptr
   std::shared_ptr<NetworkInterface> interface( const size_t N ) { return _interfaces.at( N ); }
 
-  // Add a route (a forwarding rule)
+  //  构建一个新的路由项，添加到route_table_中
+  //  route_prefix : 本质就是一个ipv4地址
+  //  prefix_length ： 子网掩码1的位数
+  //  next_hop : 接口编号对应的 Address
+  //  interface_num : 接口编号
   void add_route( uint32_t route_prefix,
                   uint8_t prefix_length,
                   std::optional<Address> next_hop,
                   size_t interface_num );
 
-  // Route packets between the interfaces
+  //  处理router所有网络接口接收到的 InternetDatagram，
+  //  能够与router_table_匹配的就通过相应接口转发
+  //  不能匹配的就丢弃
   void route();
 
 private:
-  // The router's collection of network interfaces
+  struct prefix_info
+  {
+    //  子网掩码
+    uint32_t mask_; 
+
+    //  IPv4地址中的网络部分
+    uint32_t netID_;
+
+    //  prefix_length : 前缀长度(前多少位是 1 )
+    //  prefix : 前缀(一个完整的ipv4地址)
+    //  构造函数 : 传入1个ipv4地址 prefix 和 前缀长度 prefix_length，计算出mask_ 和 netID_
+    explicit prefix_info( const uint8_t prefix_length, const uint32_t prefix )
+      : mask_ { ~( UINT32_MAX >> ( prefix_length ) ) }, netID_ { prefix & mask_ } // 只记录网络号
+    {}
+
+    //  先比较子网掩码mask_和后比较网络netID_降序排列
+    auto operator<=>( const prefix_info& other ) const
+    {
+      return other.mask_ != mask_ ? 
+               mask_ <=> other.mask_
+                                  : netID_ <=> other.netID_;
+    }
+  };
+
+  //  路由表的数据结构类型
+  using routerT = std::multimap<prefix_info, std::pair<size_t, std::optional<Address>>, std::greater<prefix_info>>;
+  
+  //  返回router_table_中与target_dst匹配 的路由项
+  routerT::const_iterator find_export( const uint32_t target_dst ) const;
+
+  //  路由表，（按子网掩码长度）降序排序所有路由条目
+  routerT router_table_ {};
+
+  //  存储一组指向NetworkInterface的shared_ptr
   std::vector<std::shared_ptr<NetworkInterface>> _interfaces {};
 };
